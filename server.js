7ad25819c35d5cd09b5cf690fb421b1032589aed
Chorna Yevheniia <chornaja.evgenija@gmail.com>
2023-04-09 20:58:09 +0300

    move DB_HOST to env

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const monggose = require("mongoose");
-
-const { DB_HOST } = process.env;
 const app = require("./app");
+// require("dotenv").config();
+const { DB_HOST } = process.env;
+
+console.log(DB_HOST);
 
 monggose
   .connect(DB_HOST)
-  .then(() => app.listen(3000))
+  .then(
+    () => app.listen(3000),
+    () => {
+      console.log("Server running. Use our API on port:3000");
+    }
+  )
   .catch((error) => console.log(error.message));
